d4d87b338a56df4275a139f9e9e24add5cbef12c
Maxim Veksler <mveksler@WW-IL-DIG2.local>
2016-04-18 14:20:02 +0300

    Register lesson 3

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-ReactDOM.render(<App />, document.getElementById('app'))
+//import App from './App'
+
+class App extends React.Component {
+  render(){
+    return <h1>{this.props.txt}</h1>
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default text',
+
+}
+ReactDOM.render(
+  <App cat={5} OTHERtxt="this is the props text"/>,
+  document.getElementById('app')
+)
