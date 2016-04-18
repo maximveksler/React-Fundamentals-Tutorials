f83344885100eb5159ec3c16dc248b8f4daa128d
Maxim Veksler <mveksler@WW-IL-DIG2.local>
2016-04-18 14:34:33 +0300

    Register lesson 5

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state =  {
+      txt: "this is the state txt",
+      cat: 0
+   }
+  }
+
+  maxSupdate(e){
+    this.setState({txt: e.target.value})
+  }
+
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
+        <input type="text"
+          onChange={this.maxSupdate.bind(this)} />
+        <h1>{this.state.txt}</h1>
         <b>Bold</b>
       </div>
     )
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import App from './App'
+import App from './App'
 
-class App extends React.Component {
-  render(){
-    return <h1>{this.props.txt}</h1>
-  }
-}
+// class App extends React.Component {
+//   render(){
+//     return <h1>{this.props.txt}</h1>
+//   }
+// }
 
 App.propTypes = {
   txt: React.PropTypes.string,
