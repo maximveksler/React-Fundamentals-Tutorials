00e9daf512278ed8d07f792fd4bb58bd426a6250
Maxim Veksler <mveksler@WW-IL-DIG2.local>
2016-04-18 18:01:16 +0300

    Register lesson 10

--- App.js
+++ App.js
@@ -14,16 +14,19 @@ class App extends React.Component {
 
   componentWillMount(){
     console.log('mounting')
+    this.setState({m: 2})
   }
   render(){
     console.log('rendering')
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
   componentDidMount(){
-    console.log('mounted')
+    console.log(ReactDOM.findDOMNode(this))
+    this.inc = setInterval(this.update, 500)
   }
   componentWillUnmount(){
     console.log('bye!');
+    clearInterval(this.inc)
   }
 }
 
