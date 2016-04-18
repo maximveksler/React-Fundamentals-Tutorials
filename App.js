ab6f0293a5128d1c996021d3157f3097af6587b8
Maxim Veksler <mveksler@WW-IL-DIG2.local>
2016-04-18 13:53:37 +0300

    Registered lesson 2

--- App.js
+++ App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <div>Hi</div>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+    // return React.createElement('h1', null, "Hello Guys")
   }
 }
+
+// statless function:
+//const App = () => <h1>Hello Eggheads</h1>
 
 export default App
